81f8d3fee367490793f526863fcf6000f1dfd759
SamuelePolimi <samuele.tosatto@gmail.com>
2016-05-23 08:24:43 +0200

    new working expman_setup

--- expman_setup.sh
+++ expman_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PATH="$PATH:$HOME/ExpMan/bin"
-mkdir $HOME/ExpMan/bin
 chmod +x $PWD/ExpMan/expman.py
-ln -s $PWD/ExpMan/expman.py $HOME/ExpMan/bin/expman
+if [ ! -f /bin/expman ]; then	
+	sudo ln -s $PWD/ExpMan/expman.py /bin/expman
+fi
 
 
